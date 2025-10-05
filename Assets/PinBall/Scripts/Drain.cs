0a50ad8aa474864241828c728ceaf864314abb12
Rodya <rodyatmb@gmail.com>
2025-10-06 00:00:59 +0300

    Drain

---
+++ Assets/PinBall/Scripts/Drain.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class Drain : MonoBehaviour
+{
+
+    [SerializeField] private Transform spawnTransfrom;
+	[SerializeField] private GameObject ballPrefab;
+
+    private BoxCollider trigger;
+
+	private void Awake()
+	{
+		trigger = GetComponent<BoxCollider>();
+		trigger.isTrigger = true;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.GetComponent<Ball>() && GameManager.BallCount > 0)
+		{
+			GameManager.BallCount -= 1;
+			Instantiate(ballPrefab, spawnTransfrom.position, Quaternion.identity);
+		}
+		Destroy(other.gameObject);
+	}
+
+}
---
+++ Assets/PinBall/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    public static UnityEvent GlobalGameOver = new UnityEvent();
+
+    public static int BallCount = 3;
+    public static int Score;
+
+    
+
+
+}
